216e85c340aa0004951f6665c44569662a629a41
Jacob Chae <jacob.chae@mobiledefense.com>
2016-03-01 19:52:07 -0500

    fixup! Ensure it is possible to delete scheduled messages

--- spec/features/delete_scheduled_messages_spec.rb
+++ spec/features/delete_scheduled_messages_spec.rb
@@ -1,16 +1,30 @@
 require "rails_helper"
 
 feature "Delete scheduled messages" do
-  scenario "from list of scheduled messages", :js do
-    message = create(:scheduled_message)
+  context "admin user" do
+    scenario "from list of scheduled messages", :js do
+      admin = create(:admin)
+      message = create(:scheduled_message)
 
-    login_with_oauth
-    visit scheduled_messages_path
-    click_accept_on_javascript_popup do
-      page.find(".button-delete").click
+      login_with_oauth(admin)
+      visit scheduled_messages_path
+      click_accept_on_javascript_popup do
+        page.find(".button-delete").click
+      end
+
+      expect(page).to have_content("You deleted #{message.title}")
     end
+  end
 
-    expect(page).to have_content("You deleted #{message.title}")
+  context "non-admin user" do
+    scenario "does not see link to destroy scheduled messages", :js do
+      user = create(:user)
+      message = create(:scheduled_message)
+
+      login_with_oauth(user)
+      visit scheduled_messages_path
+
+      expect(page).not_to have_content("Delete")
+    end
   end
 end
-
