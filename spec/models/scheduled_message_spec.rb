7b74e3287ce86a7326c55c3553764d5845b03d84
Jacob Chae <jacob.chae@mobiledefense.com>
2016-03-07 18:10:37 -0500

    Ensure that messages can be sorted by date and time

--- spec/models/scheduled_message_spec.rb
+++ spec/models/scheduled_message_spec.rb
@@ -12,4 +12,35 @@ describe ScheduledMessage do
      it { should validate_presence_of(:time_of_day) }
      it { should validate_presence_of(:title) }
   end
+
+  describe '.date_time_ordering' do
+    let(:tags) { ['testing', 'testing2'] }
+    let!(:message1) do
+      ScheduledMessage.create!(title: 'test1',
+                               body: 'this is the first test',
+                               days_after_start: 1,
+                               time_of_day: '2000-01-01 16:00:00 UTC',
+                               tag_list: tags)
+    end
+
+    let!(:message2) do
+      ScheduledMessage.create!(title: 'test2',
+                               body: 'this is the second test',
+                               days_after_start: 1,
+                               time_of_day: '2000-01-01 14:00:00 UTC',
+                               tag_list: tags)
+    end
+
+    let!(:message3) do
+      ScheduledMessage.create!(title: 'test3',
+                               body: 'this is the third test',
+                               days_after_start: 1,
+                               time_of_day: '2000-01-01 11:00:00 UTC',
+                               tag_list: tags)
+    end
+
+    it 'should display the messages in chronological order' do
+      expect(ScheduledMessage.date_time_ordering.first).to eq(message3)
+    end
+  end
 end
